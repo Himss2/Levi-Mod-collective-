15bfbac8c717bca9bd1e7ae089034ee73eb2543f
Fathul <97374618+Himss2@users.noreply.github.com>
2026-08-19 14:05:25 +0700

    Create Ids.hpp

---
+++ include/levivisuals/Ids.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace levivisuals::ids {
+
+inline constexpr char ViewModelModule[] =
+    "levivisuals.viewmodel";
+
+inline constexpr char FreelookModule[] =
+    "levivisuals.freelook";
+
+inline constexpr char ItemPhysicsModule[] =
+    "levivisuals.itemphysics";
+
+inline constexpr char FreelookButton[] =
+    "levivisuals.freelook.button";
+
+inline constexpr char ViewModelItemFov[] =
+    "itemFov";
+
+inline constexpr char FreelookSensitivity[] =
+    "sensitivity";
+
+inline constexpr char ItemPhysicsSpeed[] =
+    "speed";
+
+inline constexpr char ItemPhysicsSmooth[] =
+    "smoothRotations";
+
+inline constexpr char ItemPhysicsPreserve[] =
+    "preserveRotations";
+
+} // namespace levivisuals::ids
